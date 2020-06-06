92c6cf3f8e87daf4a5e4b51017113ee5bfd3fe17
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-06-05 21:10:46 -0300

    fix: change jest to ignore protocols on coverage

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: [
-      '<rootDir>/src/**/*.ts',
-    ],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'node',
-    transform: {
-      '.+\\.ts$': 'ts-jest'
-    }
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
   }
+}
