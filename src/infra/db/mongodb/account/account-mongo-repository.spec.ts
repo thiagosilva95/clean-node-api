26c8a2717246b1540a6e97259c8a218233a1fce4
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-06-30 22:15:18 -0300

    test: ensure AccountMongoRepository returns an account on loadByToken without role success

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -22,7 +22,7 @@ describe('Account Mongo Repository', () => {
     return new AccountMongoRepository()
   }
 
-  describe('add(', () => {
+  describe('add()', () => {
     test('Should return an account on add success', async () => {
       const sut = makeSut()
       const account = await sut.add({
@@ -38,7 +38,7 @@ describe('Account Mongo Repository', () => {
     })
   })
 
-  describe('loadByEmail(', () => {
+  describe('loadByEmail()', () => {
     test('Should return an account on loadByEmail success', async () => {
       const sut = makeSut()
       await accountCollection.insertOne({
@@ -61,7 +61,7 @@ describe('Account Mongo Repository', () => {
     })
   })
 
-  describe('updateAccessToken(', () => {
+  describe('updateAccessToken()', () => {
     test('Should update the account accessToken on updateAccessToken succeed', async () => {
       const sut = makeSut()
       const res = await accountCollection.insertOne({
@@ -75,6 +75,24 @@ describe('Account Mongo Repository', () => {
       const account = await accountCollection.findOne({ _id: res.ops[0]._id })
       expect(account).toBeTruthy()
       expect(account.accessToken).toBe('any_token')
+    })
+  })
+
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken without role success', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
   })
 })
